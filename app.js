1dcb629297aa83d7ddae71a7dc3f05860986b9c1
Joseph <Josephsipiorski@gmail.com>
2021-05-24 14:08:40 -0500

    working user model

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ const User = require('./models/User');
 const users = require('./routes/api/users');
 const bodyParser = require('body-parser');
 
+
+
 mongoose
-  .connect(db, { useUnifiedTopology: true })
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true  })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  const user = new User({
+    email: "testing@email",
+    password: "123456"
+  });
+  debugger
+  user.save()
+  debugger
+  res.send("Hello World");
+});
+
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-app.use("/api/users", users)
+app.use("/api/users", users);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,19 +3,24 @@ const User = require('../../models/User');
 const express = require("express");
 const router = express.Router();
 
+const keys = require('../../config/keys');
+
+
+
+
 router.post('/register', (req, res) => {
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
                 // Throw a 400 error if the email address already exists
-                return res.status(400).json({ email: "A user has already registered with this address" })
+                return res.status(400).json({ email: "A user has already registered with this address" });
             } else {
                 // Otherwise create a new user
                 const newUser = new User({
                     email: req.body.email,
                     password: req.body.password
-                })
+                });
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -24,10 +29,10 @@ router.post('/register', (req, res) => {
                         newUser.save()
                             .then(user => res.json(user))
                             .catch(err => console.log(err));
-                    })
-                })
+                    });
+                });
             }
-        })
-})
+        });
+});
 
 module.exports = router;
