42d4dcaaffcf7f85b4fd79b4a3400b39a3e98428
Joseph <Josephsipiorski@gmail.com>
2021-05-24 21:28:00 -0500

    complete login functionality on backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,10 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 const jwt = require('jsonwebtoken');
-// const passport = require('passport');
+const passport = require('passport');
+
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 // router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
 //   res.json({
@@ -15,6 +18,13 @@ const jwt = require('jsonwebtoken');
 // })
 
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -43,21 +53,40 @@ router.post('/register', (req, res) => {
 
 
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
-
     User.findOne({ email })
-        .the(user => {
+        .then(user => {
             if(!user){
                 return res.status(404).json({ email: "This user does not exist"});
             }
 
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
-
                     if(isMatch){
-                        res.json({ msg: "Success PW was correct"});
+                        const payload = {
+                            id: user.id,
+                            email: user.email
+                        };
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Beare " + token
+                                });
+                            }
+                        );
                     } else {
                         return res.status(400).json( {password: "Incorrect password"});
                     }
