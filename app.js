16fd0321d710ba80ce29bdfbc43e9e9b185d9c68
ShenQi1996 <qisam1989@gmail.com>
2021-05-26 10:26:03 -0400

    push to heroku

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI;
+// const db = require("./config/keys").mongoURI;
 const app = express();
-const User = require('./models/User');
-const users = require('./routes/api/users');
-const bodyParser = require('body-parser');
-const Event = require('./models/Event');
-const events= require('./routes/api/events')
+const User = require("./models/User");
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
+const Event = require("./models/Event");
+const events = require("./routes/api/events");
 
+const path = require("path");
 
-
-mongoose
-.connect(db, { useUnifiedTopology: true, useNewUrlParser: true  })
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
-
+mongoose;
+// .connect(db, { useUnifiedTopology: true, useNewUrlParser: true  })
+// .then(() => console.log("Connected to MongoDB successfully"))
+// .catch(err => console.log(err));
 
 // app.use(passport.initialize());
 // require('./config/passport')(passport);
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   // const user = new User({
@@ -31,14 +35,15 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
-
 
 app.use("/api/users", users);
 
-app.use('/api/events', events);
+app.use("/api/events", events);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
