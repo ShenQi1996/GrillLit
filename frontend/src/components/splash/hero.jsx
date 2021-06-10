8b6b025b16caeb8362345b40e7686ae0411fdd00
Joseph <Josephsipiorski@gmail.com>
2021-06-10 17:06:58 -0500

    add styled splash page

--- frontend/src/components/splash/hero.jsx
+++ frontend/src/components/splash/hero.jsx
@@ -5,6 +5,13 @@ import { Link } from 'react-router-dom';
 class Hero extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handelClick = this.handelClick.bind(this);
+  }
+
+  handelClick(e){
+    const about = document.querySelector(".home_wrapper")
+    about.classList.toggle("open")
   }
 
   render() {
@@ -15,6 +22,35 @@ class Hero extends React.Component {
         </figure>
         <section className="hero-section">
           <h1>Come Together</h1>
+
+          <Link className="link-new-a" to="/new">Plan and organize your next group picnic here</Link>
+
+          <div>
+            <button className="link-new-b" onClick={ this.handelClick }> About </button>
+          </div>
+
+        <div className="home_wrapper">
+
+          <div className="title-about">You’re just steps away 
+          <br/> from your next barbecue</div> 
+
+          <line/>
+
+          <ol className="list-home">
+
+            <p> 1. Browse</p>
+            <div>Start by exploring barbecues in your area and see what you can contribute to the party</div>
+
+            <p>2. Join</p>  
+            <div>Sign up to attend a barbecue and bring your dish</div>
+
+            <p>3. Go</p> 
+            <div>Connect with friends who share your love of grilling</div>
+
+          </ol>
+
+
+        </div>
         </section>
 
       </>
